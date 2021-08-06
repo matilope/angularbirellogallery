2a1afdd58abb648938f1e5cd6e57597d60ba417d
matilope <66441937+matilope@users.noreply.github.com>
2021-08-06 01:58:40 -0300

    Update instagram.service.ts

--- src/app/services/instagram.service.ts
+++ src/app/services/instagram.service.ts
@@ -13,7 +13,7 @@ export class InstagramService {
     }
 
     getInstagram():Observable<any> {
-        return this._http.get("https://graph.instagram.com/v1.0/17841403549294920/media?access_token=IGQVJWYno2d2RTei1uSGNTemdwU2tKZAjhUTWJia2J6bU5XX2dpLTd2WlYxdGxVbnNYdE00cG5vTkZAkLUhfa19GcTRKTUdsWkl5Q3dTSk4xUFZAZAMlVrQU9hZATJUUHBxdDM0TmhsTThHNW9JWDNORG5zYwZDZD&pretty=1&fields=caption%2Cmedia_url%2Cmedia_type%2Cpermalink%2Ctimestamp%2Cusername&limit=200")
+        return this._http.get("https://graph.instagram.com/v1.0/17841403549294920/media?access_token=************************&pretty=1&fields=caption%2Cmedia_url%2Cmedia_type%2Cpermalink%2Ctimestamp%2Cusername&limit=200")
     }
     
 }
